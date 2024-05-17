220ce0d50514a39bff98bacb21c9aa74e3d1c7d6
YADIER_GONZALEZ_GRACIANO <a.21308051280488@cbtis128.edu.mx>
2024-05-17 17:46:35 +0000

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'SuperMercado Gonzalez'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  const MyHomePage({super.key, required this.title});  
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> imgList = [
+    "https://raw.githubusercontent.com/Yadier-Gonzalez-Graciano/img-flutterflow_ios_6j/main/Act%2013%20GridView%20Flutter%20Flow/carne4.jpg",
+    "https://raw.githubusercontent.com/Yadier-Gonzalez-Graciano/img-flutterflow_ios_6j/main/Act%2013%20GridView%20Flutter%20Flow/fruta6.jpg",
+    "https://raw.githubusercontent.com/Yadier-Gonzalez-Graciano/img-flutterflow_ios_6j/main/Act%2013%20GridView%20Flutter%20Flow/fruta7.jpg",
+    "https://raw.githubusercontent.com/Yadier-Gonzalez-Graciano/img-flutterflow_ios_6j/main/Act%2013%20GridView%20Flutter%20Flow/panssssssssssss.png",
+    "https://raw.githubusercontent.com/Yadier-Gonzalez-Graciano/img-flutterflow_ios_6j/main/Act%2013%20GridView%20Flutter%20Flow/frutas6.jpg",
+  ];
+
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Column(
+        children: [
+          CarouselSlider(
+            items: imgList
+                .map((e) => Center(
+                      child: Image.network(e),
+                    ))
+                .toList(),
+            options: CarouselOptions(
+              initialPage: 0,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 2),
+              enlargeCenterPage: true,
+              enlargeFactor: 0.3,
+              onPageChanged: (value, _) {
+                setState(() {
+                  _currentPage = value;
+                });
+              },
+            ),
+          ),
+          buildCarouselIndicator(),
+        ],
       ),
+    );
+  }
+
+  Widget buildCarouselIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < imgList.length; i++)
+          Container(
+            margin: EdgeInsets.all(5),
+            height: i == _currentPage ? 7 : 5,
+            width: i == _currentPage ? 7 : 5,
+            decoration: BoxDecoration(
+              color: i == _currentPage ? Colors.black : Colors.grey,
+              shape: BoxShape.circle,
+            ),
+          ),
+      ],
     );
   }
 }
